7a3f5a01e53915b298a09bb359cfd64d800e142c
Sourjyabrata Dey <sourjya.dey2004@gmail.com>
2025-08-03 16:26:36 +0530

    Create Basic Email Slicer.py

---
+++ Basic Email Slicer.py
@@ -0,0 +1,12 @@
+# E-mail Slicer
+#--------------
+
+email = 'sourjya.dey2004@gmail.com'
+
+info = email.split('@')
+
+username, domain = info
+
+print('Username : ', username)
+print('Domain : ', domain)
+print('\n')
