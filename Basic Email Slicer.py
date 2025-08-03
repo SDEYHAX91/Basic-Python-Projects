477b4521be5d68a8e6994ca4ce91741ebe4ef308
Sourjyabrata Dey <sourjya.dey2004@gmail.com>
2025-08-03 16:36:09 +0530

    Update Basic Email Slicer.py

--- Basic Email Slicer.py
+++ Basic Email Slicer.py
@@ -1,12 +1,17 @@
 # E-mail Slicer
 #--------------
 
-email = 'sourjya.dey2004@gmail.com'
+import re
 
-info = email.split('@')
+email = input("Enter E-mail: ").strip()
 
-username, domain = info
+# Simple validation using regex
+pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
 
-print('Username : ', username)
-print('Domain : ', domain)
-print('\n')
+if re.match(pattern, email):
+    username, domain = email.split('@')
+    print('Valid Email!')
+    print('Username :', username)
+    print('Domain   :', domain)
+else:
+    print('Invalid Email!')
